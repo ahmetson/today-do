c2c6988760e3c70e86e142c5e79e87cbe202bb64
Medet Ahmetson <ahmetson@zoho.com>
2023-09-08 16:29:19 +0400

    Update the cli to test the service

---
+++ cli/go.mod
@@ -0,0 +1,18 @@
+module github.com/ahmetson/today-do/cli
+
+go 1.21rc2
+
+require (
+	github.com/ahmetson/client-lib v0.0.0-20230908110757-5f62078bd7bd // indirect
+	github.com/ahmetson/common-lib v0.0.0-20230825195825-e7b69549b9a1 // indirect
+	github.com/ahmetson/config-lib v0.0.0-20230908114625-10d96d822cde // indirect
+	github.com/ahmetson/os-lib v0.0.0-20230908110839-83535270d872 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/ethereum/go-ethereum v1.12.2 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/pebbe/zmq4 v1.2.10 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ cli/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ahmetson/client-lib"
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/ahmetson/common-lib/message"
+	"github.com/ahmetson/os-lib/arg"
+	"github.com/pebbe/zmq4"
+	"strconv"
+	"time"
+)
+
+func main() {
+	exist := arg.FlagExist("cmd")
+	if !exist {
+		panic("flag --cmd=<> not set")
+	}
+	if !arg.FlagExist("port") {
+		panic("flag --port not set")
+	}
+
+	cmd := arg.FlagValue("cmd")
+	port := arg.FlagValue("port")
+
+	title := time.Now().String()
+	description := fmt.Sprintf("this is the text that was set on %s", title)
+	backend, err := client.NewRaw(zmq4.REP, fmt.Sprintf("tcp://localhost:%s", port))
+	if err != nil {
+		panic(err)
+	}
+	backend.Timeout(time.Second)
+	backend.Attempt(1)
+
+	fmt.Printf("executing\n")
+
+	if cmd == "add" {
+		params := key_value.Empty().Set("title", title).Set("description", description)
+
+		req := message.Request{
+			Command:    "add",
+			Parameters: params,
+		}
+
+		fmt.Printf("request to the backend\n")
+
+		reply, err := backend.Request(&req)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("replied: %v\n", reply)
+	} else if cmd == "done" {
+		if !arg.FlagExist("number") {
+			panic("flag --number not set")
+		}
+
+		numberStr := arg.FlagValue("number")
+		number, err := strconv.Atoi(numberStr)
+		if err != nil {
+			panic(err)
+		}
+
+		params := key_value.Empty().Set("number", number)
+
+		req := message.Request{
+			Command:    "done",
+			Parameters: params,
+		}
+
+		fmt.Printf("request to the backend\n")
+
+		reply, err := backend.Request(&req)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("replied: %v\n", reply)
+	} else if cmd == "list" {
+		req := message.Request{
+			Command:    "list",
+			Parameters: key_value.Empty(),
+		}
+
+		reply, err := backend.Request(&req)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("replied: %v\n", reply)
+	}
+}
